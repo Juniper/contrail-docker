ba4a164ac1c542e90aaf9c72f5def0c2ab48d932
hkumarmk <hkumar@d4devops.org>
2016-09-02 15:27:12 -0700

    Contrail vrouter module compiler container code

---
+++ docker/vrouter_module_compiler/redhat/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -x
+function fail() {
+    echo "$@"
+    exit 1
+}
+
+KERNEL_VERSION=$(uname -r)
+VROUTER_VERSION=$(rpm -q contrail-vrouter-source | sed -r 's/contrail-vrouter-source-([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\-[[:digit:]]+).*/\1/')
+VROUTER_MODULE_PATH=${VROUTER_MODULE_PATH:-"/opt/contrail/vrouter_modules/"}
+VROUTER_MODULE_SAVE_PATH=${VROUTER_MODULE_PATH}/${KERNEL_VERSION}/${VROUTER_VERSION}
+if [[ ! -e /lib/modules/${KERNEL_VERSION} ]]; then
+    fail "No kernel module directory found under /lib/modules for current kernel ($KERNEL_VERSION)"
+fi
+
+if [[ ! -e /usr/src/kernels/$KERNEL_VERSION ]]; then
+    fail "No kernel build directory found under /usr/src/kernels/ for current kernel ($KERNEL_VERSION)"
+fi
+
+cd /usr/src/modules/contrail-vrouter/
+tar zxvf contrail-vrouter*.tar.gz
+make
+[[ -e $VROUTER_MODULE_SAVE_PATH ]] || mkdir -p $VROUTER_MODULE_SAVE_PATH
+cp vrouter.ko $VROUTER_MODULE_SAVE_PATH || fail "Failed copying vrouter module to $VROUTER_MODULE_SAVE_PATH"
+echo "Vrouter kernel module is copied to $VROUTER_MODULE_SAVE_PATH"
