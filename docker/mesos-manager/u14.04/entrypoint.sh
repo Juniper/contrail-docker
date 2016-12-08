ace107560fa2dd541c89a630e9427f1ce30ef65f
hkumarmk <hkumar@d4devops.org>
2016-12-09 01:51:12 +0530

    Fix mesos-manager dockerfile

--- docker/mesos-manager/u14.04/entrypoint.sh
+++ docker/mesos-manager/u14.04/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -x
-source /common.sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 DAEMON=/usr/bin/supervisord
-CONFIG=/etc/contrail/contrail-kubernetes.conf
-SERVICE=kubernetes
+SERVICE=mesos
 ANSIBLE_INVENTORY=${ANSIBLE_INVENTORY:-"all-in-one"}
 
 test -x $DAEMON || exit 0
@@ -15,5 +13,5 @@ ulimit -c unlimited
 ulimit -d unlimited
 ulimit -v unlimited
 ulimit -n 4096
-RUN contrailctl config sync -c mesosmanager -F -t configure,provision
+contrailctl config sync -c mesosmanager -F -t configure,provision
 $DAEMON $DAEMON_OPTS
