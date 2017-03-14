daa803d8f85fb6b0c15ab60304b319e68f550802
hkumarmk <hkumar@d4devops.org>
2017-03-14 13:20:21 -0700

    Added options to config node add

--- tools/python-contrailctl/contrailctl/config.py
+++ tools/python-contrailctl/contrailctl/config.py
@@ -3,6 +3,7 @@ import ast
 import re
 import os
 
+
 def read_config(config_file):
     config = ConfigParser.ConfigParser()
     config.read(config_file)
@@ -20,11 +21,14 @@ class Configurator(object):
         self.param_map = param_map
 
     @staticmethod
-    def _eval(data):
-        if re.match(r"^\[.*\]$", data) or re.match(r"^\{.*\}$", data):
-            return ast.literal_eval(data)
-        elif data.lower() in ("yes", "true", "no", "false"):
-            return data.lower() in ("yes", "true")
+    def eval(data):
+        if isinstance(data, str):
+            if re.match(r"^\[.*\]$", data) or re.match(r"^\{.*\}$", data):
+                return ast.literal_eval(data)
+            elif data.lower() in ("yes", "true", "no", "false"):
+                return data.lower() in ("yes", "true")
+            else:
+                return data
         else:
             return data
 
@@ -34,15 +38,15 @@ class Configurator(object):
         Configurator in which case this config_dict should not be overwritten, so it always get updated
         :return: loaded config_dict
         """
-        cctl_envs={k.replace('CCTL_','').lower(): self._eval(v) for k, v in os.environ.items() if 'CCTL_' in k}
+        cctl_envs={k.replace('CCTL_','').lower(): self.eval(v) for k, v in os.environ.items() if 'CCTL_' in k}
         for section in self.master_config.sections():
             for param, value in self.master_config.items(section):
                 if param in self.param_map.get(section, {}):
-                    config_dict.update({self.param_map[section][param]: self._eval(value)})
+                    config_dict.update({self.param_map[section][param]: self.eval(value)})
                 elif section == 'GLOBAL':
-                    config_dict.update({param: self._eval(value)})
+                    config_dict.update({param: self.eval(value)})
                 else:
-                    config_dict.update({"{}_{}".format(section.lower(), param): self._eval(value)})
+                    config_dict.update({"{}_{}".format(section.lower(), param): self.eval(value)})
 
-        cctl_envs.update(config_dict)
-        return cctl_envs
+        config_dict.update(cctl_envs)
+        return config_dict
