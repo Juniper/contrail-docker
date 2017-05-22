5a685d477f0172b15d9a3fd71760abc599b81e4f
Yuvaraja Mariappan <ymariappan@juniper.net>
2017-05-22 00:27:59 -0700

    Revert "Add flow_export_rate in controller JSON schema"
    This reverts commit d87e3c008283c54c5e4aa4998a229612773b4c53.
    bug 1688525 has to be solved seperately.
    
    Change-Id: Ib5901c212d1a599af7a0cafacf2ec7cf60e8f030
    Closes-bug: #1692357

--- tools/python-contrailctl/contrailctl/config.py
+++ tools/python-contrailctl/contrailctl/config.py
@@ -1,5 +1,5 @@
 import ConfigParser
-import yaml
+import ast
 import re
 import os
 
@@ -24,11 +24,12 @@ class Configurator(object):
     @staticmethod
     def eval(data):
         if isinstance(data, str):
-            try:
-                data = yaml.load(data)
-            except:
-                pass
-            return data
+            if re.match(r"^\[.*\]$", data) or re.match(r"^\{.*\}$", data):
+                return ast.literal_eval(data)
+            elif data.lower() in ("yes", "true", "no", "false"):
+                return data.lower() in ("yes", "true")
+            else:
+                return data
         else:
             return data
 
