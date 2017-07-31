c5b9f010c8c8e68ea161703c7a2b28036f55cf93
madhukar32 <mnayakbomman@juniper.net>
2017-07-30 22:27:12 -0700

    Best effort waiting for k8s volumes to be mounted
    Best effort waiting for 180 secs for k8s volume to be mounted. Taking
    care of comments given in review#
    34100(https://review.opencontrail.org/#/c/34100/)
    
    Closes-Bug: #1706381
    Change-Id: If7b8f982b3f9a9d286e541423a0d0ccb6b20d681

--- tools/python-contrailctl/contrailctl/k8s_modify_config.py
+++ tools/python-contrailctl/contrailctl/k8s_modify_config.py
@@ -10,7 +10,6 @@ AGENT_DATA = ("global", "agent")
 KUBEMANAGER_DATA = ("global", "kubemanager")
 KUBERNETESAGENT_DATA = ("global", "kubernetesagent")
 
-
 DUMMY_IP = "1.1.1.1"
 DUMMY_PORT = 80
 
@@ -22,38 +21,44 @@ class K8sModifyConfig(object):
         """
         self.component = contrail_role
         self.tmp_conf_dir = tmp_conf_dir
+        self.config_file = config_file
 
         if not self.tmp_conf_dir.endswith("/"):
             self.tmp_conf_dir += "/"
 
-        self._get_filename = lambda file_name: self.tmp_conf_dir + file_name + ".conf"
+        self._get_filename = lambda data_name: self.tmp_conf_dir + data_name + ".conf"
 
         if not os.path.exists(CONTRAILCTL_DIR):
             os.makedirs(CONTRAILCTL_DIR)
-
-        self.contrailctl_conf = open(config_file, 'w')
 
     def _get_config_from_files(self, file_list):
         """ Reads ini config from the input file list provided and
             returns ConfigParser object
         """
-        config_object = ConfigParser.ConfigParser()
+        if type(file_list) is not list:
+            file_list = [file_list]
+        # Waiting for volume files /tmp/contrailctl/* to be mounted
         for count in range(180):
-            try:
-                if config_object.read(file_list):
-                    break
-            except ConfigParser.Error as e:
-                print("Error while reading the config file: %s\n%s"
-                      %(filename,e))
+            file_exists_list = [os.path.exists(fname) for fname in file_list]
+            if False in file_exists_list:
+                time.sleep(1)
+                continue
+            else:
                 break
-            time.sleep(1)
+        if False in file_exists_list:
+            print("One of the file in %s does not exist" %file_list)
+            return False
 
+        config_object = ConfigParser.ConfigParser()
+        config_object.read(file_list)
         return config_object
 
     def merge_update_sections_agent(self):
         """ Merges the section and update for agent role """
         self.config_files = map(self._get_filename, AGENT_DATA)
         agent_config = self._get_config_from_files(self.config_files)
+        if not agent_config:
+            return False
         if not agent_config.has_option("AGENT", "ctrl_data_network"):
             pod_ip = self._get_pod_ip()
             agent_config.set("AGENT", "ctrl_data_ip",value=pod_ip)
@@ -64,24 +69,31 @@ class K8sModifyConfig(object):
         agent_config.set("KUBERNETES", "api_server",value=k8s_api_server)
         agent_config = self._update_k8s_token(agent_config, "KUBERNETES")
         self._write_contrailctl_file(agent_config)
+        return True
 
     def merge_update_sections_kubemanager(self):
         """ Merges the section and update for kubemanager role """
         self.config_files = map(self._get_filename,KUBEMANAGER_DATA)
         kubemanager_config = self._get_config_from_files(self.config_files)
+        if not kubemanager_config:
+            return False
         kubemanager_config = self._update_k8s_token(kubemanager_config, "KUBERNETES")
         self._write_contrailctl_file(kubemanager_config)
+        return True
 
     def merge_update_sections_kubernetesagent(self):
         """ Merges the section and update for kubernetesagent role """
         self.config_files = map(self._get_filename,KUBERNETESAGENT_DATA)
         kubernetesagent_config = self._get_config_from_files(self.config_files)
+        if not kubernetesagent_config:
+            return False
         self._write_contrailctl_file(kubernetesagent_config)
+        return True
 
     def _write_contrailctl_file(self, role_config):
         """ Write role config to the contrailcltl file """
-        role_config.write(self.contrailctl_conf)
-        self.contrailctl_conf.close()
+        with open(self.config_file, 'w') as contrailctl_file:
+            role_config.write(contrailctl_file)
 
     def _update_k8s_token(self, role_config, section):
         """ Update config_obj with token value in the given section """
@@ -104,6 +116,8 @@ class K8sModifyConfig(object):
         """ Reads kubemanager.conf to get the k8s_api_server"""
         kubemanager_file = self.tmp_conf_dir + "kubemanager.conf"
         kubemanager_config = self._get_config_from_files(kubemanager_file)
+        if not kubemanager_config:
+            return ""
         if kubemanager_config.has_option("KUBERNETES", "api_server"):
             k8s_api_server = kubemanager_config.get("KUBERNETES", "api_server")
         else:
