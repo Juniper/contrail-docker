5f73fb315f27b2c983e93bc217406f1e4458a45e
Kamlesh Parmar <kparmar@juniper.net>
2017-08-10 10:45:07 -0700

    Enable make for vcenter-plugin container and fix vcenter-plugin key to vcenterplugin
    Change-Id: I229ca73b25cd67baac4315040eb58085a158cfe2
    Partial-Bug: #1703431

--- tools/python-contrailctl/contrailctl/cmd.py
+++ tools/python-contrailctl/contrailctl/cmd.py
@@ -223,7 +223,7 @@ def main(args=sys.argv[1:]):
 
     components = ["controller", "analyticsdb", "analytics", "agent",
                       "lb","kubemanager", "mesosmanager", "cephcontroller", "contrailissu",
-                      "kubernetesagent", "vcenter-plugin"]
+                      "kubernetesagent", "vcenterplugin"]
     types = ["controller", "analyticsdb", "analytics", "cephcontroller"]
     ap_node_common = argparse.ArgumentParser(add_help=False)
     ap_node_common.add_argument('-t', '--type', type=str, required=True,
